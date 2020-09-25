18341a35d0ff8fd95dbdace7d5250586d60d5123
albanie <albanie@robots.ox.ac.uk>
2020-09-25 08:51:48 +0100

    bump + rename tests

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ for pattern in extra_package_patterns:
 
 setuptools.setup(
     name="yaspi",
-    version="0.0.1",
+    version="0.0.2",
     entry_points={
         "console_scripts": [
             "yaspi=yaspi.yaspi:main",
